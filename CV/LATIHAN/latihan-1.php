1184e46c2d66dd5d04cbd043847ba9f9e60b4123
im-afn <arifinppbu@gmail.com>
2024-11-17 11:48:44 +0700

    ubah lagi

---
+++ CV/LATIHAN/latihan-1.php
@@ -0,0 +1,23 @@
+<p><b>Penjumlahan</b></p>
+<?php
+$num1 = 10;
+$num2 = 20;
+$sum = 0;
+
+$sum = $num1 + $num2;
+
+echo "The sum of $num1 and $num2 is: $sum";
+?>
+
+<br>
+
+<p><b>Perkalian</b></p>
+<?php
+$length = 5;
+$width = 10;
+$area = 0;
+
+$area = $length * $width;
+
+echo "The area of the rectangle is: $area";
+?>
